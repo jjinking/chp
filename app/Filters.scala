2b7463cc4b8a82d2c9ba60db7cc9a3f4dcce6353
joseph.kim <joseph.kim@workday.com>
2017-06-23 01:55:46 -0700

    Set up cors for dev env because chrome considers localhost as CORS

--- app/Filters.scala
+++ app/Filters.scala
@@ -2,8 +2,7 @@ import javax.inject._
 import play.api._
 import play.api.http.HttpFilters
 import play.api.mvc._
-
-import filters.ExampleFilter
+import play.filters.cors.CORSFilter
 
 /**
  * This class configures filters that run on every request. This
@@ -21,13 +20,13 @@ import filters.ExampleFilter
 @Singleton
 class Filters @Inject() (
   env: Environment,
-  exampleFilter: ExampleFilter) extends HttpFilters {
+  corsFilter: CORSFilter) extends HttpFilters {
 
   override val filters = {
     // Use the example filter if we're running development mode. If
     // we're running in production or test mode then don't use any
     // filters at all.
-    if (env.mode == Mode.Dev) Seq(exampleFilter) else Seq.empty
+    if (env.mode == Mode.Dev) Seq(corsFilter) else Seq.empty
   }
 
 }
